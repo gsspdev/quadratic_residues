318bb2ee1f57cb5a9286327df2a233086dca49c8
gsspdev <dev@gssp.io>
2025-04-29 14:24:01 -0500

    renamed functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub mod get_residues {
     // which has been renaimed to slow. There is an n/2 optimization
     // by only computing the first half of the vector.
     // The last half mirrors the first half.
-    pub fn optimized_but_failing(number: i32) -> Vec<i32> {
+    pub fn get_residues_optimized_but_failing(number: i32) -> Vec<i32> {
         let n = number.abs();
         let ints_vec = ints_less_than_n(n);
         let midpoint = ints_vec.len() / 2;
@@ -24,37 +24,38 @@ pub mod get_residues {
         res.sort();
         res
     }
+
+    /// Gets residues, includes duplicates
+    // There is an n/2 optimization here, using default,
+    // and the mirrored output of default.
+    pub fn get_all_residues(number: i32) -> Vec<i32> {
+        let mut v = ints_less_than_n(number);
+        for x in v.iter_mut() {
+            *x = (*x * *x) % number;
+        }
+        return v;
+    }
+
     /// Gets quadratic residues
-    pub fn default(number: i32) -> Vec<i32> {
+    pub fn get_residues(number: i32) -> Vec<i32> {
         let n = number.abs();
-        let mut residues = all(n);
+        let mut residues = get_all_residues(n);
         residues.sort();
         residues.dedup();
         residues
     }
 
     /// Gets non-residues
-    pub fn non(number: i32) -> Vec<i32> {
+    pub fn get_non_residues(number: i32) -> Vec<i32> {
         let n = number.abs();
         let vector: Vec<i32> = ints_less_than_n(n);
-        let residues: Vec<i32> = default(n);
+        let residues: Vec<i32> = get_residues(n);
 
         let non_residues: Vec<i32> = vector
             .into_iter()
             .filter(|x| !residues.contains(x))
             .collect();
         non_residues
-    }
-
-    /// Gets residues, includes duplicates
-    // There is an n/2 optimization here, using default,
-    // and the mirrored output of default.
-    pub fn all(number: i32) -> Vec<i32> {
-        let mut v = ints_less_than_n(number);
-        for x in v.iter_mut() {
-            *x = (*x * *x) % number;
-        }
-        return v;
     }
 }
 
